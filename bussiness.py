95ec2366fffb75f3c1d7ab26fb59fc31e9025514
fayoue <161594383+fayoue@users.noreply.github.com>
2026-09-22 13:01:46 +0300

    Add files via upload

---
+++ bussiness.py
@@ -0,0 +1,17 @@
+# Business Purchase System
+
+# Store business and product information
+business_name = "Fay Walls"
+product_name = "wall portraits"
+product_price = 500.00
+quantity_purchased = 5
+
+# Calculate the total purchase cost
+total_cost = product_price * quantity_purchased
+
+# Display the purchase information
+print(f"Business Name: {business_name}")
+print(f"Product Name: {product_name}")
+print(f"Quantity Purchased: {quantity_purchased}")
+print(f"Product Price: {product_price}")
+print(f"Total Cost: {total_cost}")
